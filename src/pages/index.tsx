6bbe057550bf4253f43ddbe00cfa146f0224afba
Gabriel <gabriel02ps@gmail.com>
2022-04-20 16:00:55 -0300

    sign in and header component creation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
-export default function Home() {
+import { Flex, Button, Stack, Image} from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
+export default function SignIn() {
   return (
-    <h1>DashGo</h1>
+    <Flex
+      w='100vw'
+      h='100vh'
+      alignItems='center'
+      justifyContent='center'>
+      <Flex
+        as='form'
+        width='100%'
+        maxWidth={360}
+        bg='gray.800'
+        padding='8'
+        borderRadius={8}
+        flexDirection='column'
+      >
+        <Stack spacing='4'>
+          <Input name='email' type='email' label='Email' />
+
+          <Input name='password' type='password' label='Senha' />
+
+        </Stack>
+
+        <Button type='submit' marginTop='6' colorScheme='pink' size='lg'>Entrar</Button>
+      </Flex>
+
+    </Flex>
   )
 }
